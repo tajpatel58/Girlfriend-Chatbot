ba3b778d4130bfe45d821488e7871fb234197871
tajpatel58 <tajpatel58@gmail.com>
2022-05-23 22:35:57 +0100

    Creating Framework for Dataset

---
+++ Scripts/chat_bot_dataset.py
@@ -0,0 +1,20 @@
+# Import relevant packages:
+import torch 
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+from nltk import PorterStemmer
+
+class ChatbotDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self): 
+        pass
+
+    def __len__(self):
+        pass 
+    
+    def get_data(self):
+        pass 
+
