732231a916d44f91155540e27e2dab2fefd22426
tajpatel58 <tajpatel58@gmail.com>
2022-05-26 00:57:10 +0100

    Adding Entire Dataset Cleaning Function

--- Scripts/chat_bot_dataset.py
+++ Scripts/chat_bot_dataset.py
@@ -11,13 +11,14 @@ import boto3
 class ChatbotDataset(Dataset):
     def __init__(self):
         # For each datapoint we need to: drop "xoxox", tokenize, lower, stem, turn into a vector using bag_of_words.  
-        self.extract_data()
         self.ps = PorterStemmer()
+        self.extract_data()
+        self.clean_data()
+
 
 
     def __getitem__(self, idx): 
-        raw_message = self.messages_list[idx]
-        tokenized_message = word_tokenize(raw_message)
+        pass
 
     def __len__(self):
         pass 
@@ -53,6 +54,7 @@ class ChatbotDataset(Dataset):
         # Get numerical encoding of labels
         self.numeric_labels = [self.label_mapping[label] for label in self.labels]
 
+
     def clean_text(self, text):
         lowered_text = text.lower()
         list_text = lowered_text.split(' ')
@@ -61,10 +63,16 @@ class ChatbotDataset(Dataset):
         
 
     def clean_data(self):
-        # lower case all messages:
-        self.messages_list = [message.lower() for message in self.messages_list]
+        # Clean up all messages and extracting "bag" for bag of words:
+        self.bag = set()
 
-        # apply a PorterStemmer to all words. 
+        # list to hold cleaned messages:
+        self.cleaned_messages_list
+        for message in self.messages_list:
+            cleaned_message = self.clean_text(message)
+            self.cleaned_messages = self.cleaned_messages_list.append(cleaned_message)
+            self.bag.update(set(cleaned_message))
+        self.bag = list(self.bag)
 
     def bag_words(self, tokenized_text):
         pass
