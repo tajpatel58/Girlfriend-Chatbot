0c6bffc23aaab26c8850d226f1779262ecdf4879
tajpatel58 <tajpatel58@gmail.com>
2022-05-27 22:15:26 +0100

    Implementing Test for Bag of Words

--- Testing/test_chatbotDataset.py
+++ Testing/test_chatbotDataset.py
@@ -11,5 +11,10 @@ class DatasetTestCase(unittest.TestCase):
         actual = self.dataset.clean_text(text)
         self.assertEqual(expected, actual)
 
+    def test_bag(self):
+        expected_words_in_bag = ['pasta', 'hello', 'relax']
+        for word in expected_words_in_bag:
+            self.assertIn(word, self.dataset.bag)
+
 
 unittest.main(argv=[''], verbosity=2, exit=False)
