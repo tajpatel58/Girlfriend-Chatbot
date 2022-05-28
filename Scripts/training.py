a116f892aaca331800c7cd4b662a8c562837018e
tajpatel58 <tajpatel58@gmail.com>
2022-05-28 23:03:21 +0100

    Adjusting Imports and Initialising Dataloader

--- Scripts/training.py
+++ Scripts/training.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 import os 
 from chat_bot_dataset import ChatbotDataset
 from torch.utils.data import DataLoader
-
+from torch.optim import Adam
+from model import NeuralNet
 #Initialise Training Dataset:
 train_data = ChatbotDataset()
 
@@ -16,3 +17,13 @@ num_classes = train_data.num_classses
 batch_size = 6
 learning_rate = 0.001
 num_epochs = 1000
+
+# Create Dataloader to pass data in batches:
+train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
+
+#Initialise Model:
+model = NeuralNet()
+
+#Initialise Optimizer and loss(recall - CrossEntropy applies Crossmax for us):
+optimizer = Adam(model.parameters(), lr=learning_rate)
+loss = nn.CrossEntropyLoss()
