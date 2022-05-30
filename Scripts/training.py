0c5c957c9b3bad5b1c0257c227196ad59e02a839
tajpatel58 <tajpatel58@gmail.com>
2022-05-30 23:18:05 +0100

    Adding Functionality to store/save best model params

--- Scripts/training.py
+++ Scripts/training.py
@@ -1,7 +1,7 @@
 # Import Packages:
 import torch
 import torch.nn as nn
-import os 
+import copy
 from chat_bot_dataset import ChatbotDataset
 from torch.utils.data import DataLoader
 from torch.optim import Adam
@@ -27,22 +27,43 @@ num_epochs = 1000
 train_loader = DataLoader(train_data, shuffle=True, batch_size=batch_size)
 
 #Initialise Model:
-model = NeuralNet(input_size, hidden_layer_1, hidden_layer_2, num_classes)
+nn_model = NeuralNet(input_size, hidden_layer_1, hidden_layer_2, num_classes)
 
-#Initialise Optimizer and loss(recall - CrossEntropy applies Crossmax for us):
-optimizer = Adam(model.parameters(), lr=learning_rate)
+#Initialise Optimizer and loss(recall - CrossEntropy applies Softmax for us):
+optimizer = Adam(nn_model.parameters(), lr=learning_rate)
 loss = nn.CrossEntropyLoss()
 
 #Training Loop:
-for epoch in range(num_epochs):
-    for batch_no, (data, labels) in enumerate(train_loader):
-        batch_no += 1
-        predictions = model(data)
-        error = loss(predictions, labels)
-        print(error)
-        writer.add_scalar('Loss/Train', error, epoch)
-        error.backward()
-        optimizer.step()
-        optimizer.zero_grad()
+def train(model, optim, loss_func, number_of_epochs=100):
 
-writer.flush()
+    #Want to save the model with the highest accuracy on training set:
+    best_model = copy.deepcopy(model.state_dict())
+    best_acc = 0
+
+    for epoch in range(number_of_epochs):
+
+        epoch_accuracy = 0
+        model_state = copy.deepcopy(model.state_dict())
+
+        for batch_no, (data, labels) in enumerate(train_loader):
+            batch_no += 1
+            predictions = model(data)
+            error = loss_func(predictions, labels)
+            _, class_predictions = torch.max(predictions, axis=1)
+            epoch_accuracy += torch.sum(class_predictions == labels)
+            writer.add_scalar('Loss/Train', error, epoch)
+            error.backward()
+            optim.step()
+            optim.zero_grad()
+        
+        if epoch_accuracy >= best_acc: 
+            best_model = model_state
+
+    # Load the model with the highest testing accuracy
+    model.load_state_dict(best_model)
+
+    writer.flush()
+
+    return model
+
+train(nn_model, optimizer, loss, number_of_epochs=num_epochs)
