a5148f624d3ec37d0dab5781d414756b777e2d4e
tajpatel58 <tajpatel58@gmail.com>
2022-06-01 22:42:21 +0100

    Testing All Labels Read from Data

--- Testing/test_chatbotDataset.py
+++ Testing/test_chatbotDataset.py
@@ -5,11 +5,12 @@ class DatasetTestCase(unittest.TestCase):
     def setUp(self) -> None:
         self.dataset = ChatbotDataset()
     
-    def test_clean_text(self):
-        text = 'What a GREAT day'
-        expected = ['what', 'a', 'great', 'day']
-        actual = self.dataset.clean_text(text)
-        self.assertEqual(expected, actual)
+    def test_label_mapping(self):
+        labels = self.dataset.label_mapping
+        all_labels = ['greeting', 'working_day', 'feelings_or_actions', 'goodbye', 
+             'day_intents', 'food_intents', 'food_response', 
+             'morning_greeting', 'goodbye_night']
+        self.assertEqual(labels, all_labels)
 
     def test_bag(self):
         expected_words_in_bag = ['pasta', 'hello', 'relax']
